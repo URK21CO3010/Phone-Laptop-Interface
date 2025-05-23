fee414987f61c996b36d40422b36c76412f41bec
URK21CO3010 <110015139+URK21CO3010@users.noreply.github.com>
2025-05-23 22:51:55 +0530

    Update app.py to use port

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def get_latest():
     return jsonify({"text": latest_message})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
